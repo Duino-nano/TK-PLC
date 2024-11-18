2914772a1a2efba329afc745edeab7b677d650fb
Duino-nano <wsdqr907869@yahoo.co.jp>
2024-11-18 21:47:02 +0900

    new

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,27 @@
 #include "setWeb.hpp"
 #include "eeprom.hpp"
 #include "key.hpp"
-#define OUT_1   4
-#define OUT_2   16
-#define OUT_3   17
-#define OUT_4   18
-#define OUT_5   19
+#define OUT_1 4
+#define OUT_2 16
+#define OUT_3 17
+#define OUT_4 18
+#define OUT_5 19
 
-#define IN_1    26
-#define IN_2    25
-#define IN_3    33
-#define IN_4    32
-#define IN_5    35
-#define IN_6    34
+#define IN_1 26
+#define IN_2 25
+#define IN_3 33
+#define IN_4 32
+#define IN_5 35
+#define IN_6 34
 
-#define SENSOR_IN       (!digitalRead(IN_1))
-#define SET_BOTTOM      (!digitalRead(IN_6))
-#define HOPPER_IN           (!digitalRead(IN_2))
+#define CAMERA_SENSOR_IN (!digitalRead(IN_1))
+#define CAMERA_JUDGE_IN (!digitalRead(IN_2))
+#define SENSOR2_IN (!digitalRead(IN_4))
+#define SET_BOTTOM (!digitalRead(IN_6))
 // #define NG_IN           digitalRead(IN_3)
-#define BELT_OUT(x)     digitalWrite(OUT_1,x)
-#define LED_OUT(x)      digitalWrite(OUT_3,x)
+#define CAMERA_OUT(x) digitalWrite(OUT_1, x)
+#define CYLINDER_OUT(x) digitalWrite(OUT_2, x)
+#define LED_OUT(x) digitalWrite(OUT_5, x)
 // #define ALERT_OUT(x)    digitalWrite(OUT_2,x)
 // #define SENSOR_OUT(x)    digitalWrite(OUT_4,x)
 // #define Pushe_OUT(x)    digitalWrite(OUT_5,x)
@@ -48,41 +50,51 @@ void setup() {
 
 void loop() {
   static int sq = 0;
+  static IntervalTimer timer(100);
   LED_OUT(setWebMain(setKey));
-  switch(sq){
-    case 0:
-      if((SENSOR_IN)&&(HOPPER_IN)){
+
+  switch (sq) {
+    case 0:  // カメラセンサーが検知されたら
+      if (CAMERA_SENSOR_IN) {
+        sq++;
         timer.setTime(memory_data.waitTime[0]);
+      }
+      break;
+
+    case 1:  // カメラジャッジ開始
+
+      CAMERA_OUT(1);
+      sq++;
+      break;
+
+    case 2:  // カメラNGの場合は
+      if (CAMERA_JUDGE_IN) {
         sq++;
       }
-      BELT_OUT(0);
-    break;
+      break;
 
-    case 1:
-      if(timer.isWait()){
-        BELT_OUT(1);
+    case 3:  // シリンダーで押し出し
+      CYLINDER_OUT(1);
+      timer.init();
+      sq++;
+      break;
+
+    case 4:  // シリンダーが動作するまで待機
+      if (timer.isWait()) {
+        CYLINDER_OUT(0);
         sq++;
+        timer.init();
       }
-    break;
+      break;
 
-    case 2:
-      if((!SENSOR_IN)||(!HOPPER_IN)){
-        timer.setTime(memory_data.waitTime[1]);
-        sq++;
-      }
-      BELT_OUT(1);
-    break;
-
-    case 3:
-      if(timer.isWait()){
-        BELT_OUT(0);
+    case 5:  // シリンダーで押し出したか確認
+      if (SENSOR2_IN) {
         sq = 0;
+        timer.init();
       }
-    break;
+      break;
 
     default:
-    break;
+      break;
   }
-  
 }
-
